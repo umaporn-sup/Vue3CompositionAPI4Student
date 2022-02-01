c1ca9f28c731516ae21ac1dd4468a0fec01b203f
umaporn-sup <umaporn@sit.kmutt.ac.th>
2022-02-01 09:35:31 +0700

    [update] account project practice solution

--- basic-template/src/App.vue
+++ basic-template/src/App.vue
@@ -1,17 +1,53 @@
 <script setup>
-const courses = [
-  { courseId: 'INT201', courseName: 'Client I', credit: 2 },
-  { courseId: 'INT202', courseName: 'Client II', credit: 2 },
-  { courseId: 'INT210', courseName: 'Architecture and Deployment', credit: 3 }
+const accounts = [
+  {
+    name: 'salary',
+    amount: 10000
+  },
+  {
+    name: 'rent',
+    amount: -2500
+  },
+  {
+    name: 'parking fee',
+    amount: 0
+  },
+  {
+    name: 'car fuel',
+    amount: -600
+  },
+  {
+    name: 'commission',
+    amount: 5500
+  }
 ]
 </script>
 
 <template>
   <ul>
-    <li v-for="(course, propKey) in courses" :key="course.courseId">
-      {{ course.courseId }}, {{ course.credit }}
+    <li v-for="account in accounts">
+      <p v-if="account.amount !== 0">
+        <span> {{ account.name }} </span>:
+        <span
+          :style="
+            account.amount > 0
+              ? 'background-color:green'
+              : 'background-color:red'
+          "
+        >
+          {{ account.amount }}</span
+        >
+      </p>
     </li>
   </ul>
+  <p>
+    Net Total:
+    {{ accounts.reduce((total, account) => total + account.amount, 0) }}
+  </p>
+  <!-- #round 1 (0, account#1) :   0+10000 
+      #round 2 (10000, account#2): 10000+(-2500)
+      #round 3(7500, account#3):....
+  -->
 </template>
 
 <style></style>
