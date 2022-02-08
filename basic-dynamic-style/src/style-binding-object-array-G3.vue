b9576775697e8b89a5c516bbe10146f449d9003b
umaporn-sup <umaporn@sit.kmutt.ac.th>
2022-02-08 15:30:54 +0700

    [update] class and style biding from Group 3

---
+++ basic-dynamic-style/src/style-binding-object-array-G3.vue
@@ -0,0 +1,28 @@
+<script setup>
+const warningColor = 'red'
+const warningStyle = 'italic'
+const successText = 'color:green'
+const decorText = 'text-decoration:underline'
+const isEmpty = false
+const hasDecor = true
+</script>
+
+<template>
+  <div>
+    <!-- binding style (object syntax) -->
+    <p :style="{ color: warningColor, 'font-style': warningStyle }">
+      This is a warning text.
+    </p>
+    <p :style="isEmpty ? { color: 'red' } : { color: 'green' }">
+      This is a style under condition
+    </p>
+    <!-- binding style (array syntax) -->
+    <p :style="[successText]">This is a success text</p>
+    <p :style="[successText, decorText]">This is success and decoration text</p>
+    <p :style="[successText, hasDecor ? decorText : '']">
+      This is styles under condition
+    </p>
+  </div>
+</template>
+
+<style></style>
---
+++ basic-dynamic-style/src/style-class-not-binding-G3.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div>
+    <p style="color: green; text-decoration: underline">
+      This is an example text with style
+    </p>
+    <p class="example-text">This is an example text with class</p>
+  </div>
+</template>
+
+<style scoped>
+.example-text {
+  font-style: italic;
+  color: orange;
+}
+</style>
