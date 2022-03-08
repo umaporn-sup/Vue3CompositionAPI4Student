dd3d0b7e712ad2c09d56153c9661fab73cab39a7
umaporn-sup <umaporn@sit.kmutt.ac.th>
2022-03-08 10:43:23 +0700

    [update] one way data flow

---
+++ basic-comp/src/components/BaseButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, computed } from 'vue'
+const prop = defineProps({
+  yourText: {
+    type: String,
+    require: true
+  },
+  yourColor: {
+    type: String,
+    require: true
+  }
+})
+console.log(prop.yourColor)
+console.log(prop.yourText)
+// when BaseButton want to change yourText or yourColor, you need to create local variable like this:
+// 1. using ref function
+// const buttonText = ref(prop.yourText)
+// 2. using computed function
+// const buttonText = computed(() => prop.yourText)
+</script>
+
+<template>
+  <!-- <button class="button-color">{{ buttonText }}</button> -->
+  <button class="button-color">{{ yourText }}</button>
+</template>
+
+<style>
+.button-color {
+  background-color: v-bind(yourColor);
+}
+</style>
