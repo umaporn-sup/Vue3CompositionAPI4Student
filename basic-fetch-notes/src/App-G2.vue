86ab0151ecd943f725eda11e48a4aa129ce6396c
umaporn-sup <umaporn@sit.kmutt.ac.th>
2022-03-16 17:23:44 +0700

    [update] basic-fetch-notes for Group 2

---
+++ basic-fetch-notes/src/App-G2.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { ref, onBeforeMount } from 'vue'
+import CreateEditNote from './components/CreateEditNote-G2.vue'
+import NoteList from './components/NoteList-G2.vue'
+const notes = ref([])
+
+onBeforeMount(async () => {
+  await getNotes()
+  console.log(notes.value)
+})
+
+// GET
+const getNotes = async () => {
+  const res = await fetch('http://localhost:5000/notes')
+  if (res.status === 200) {
+    notes.value = await res.json()
+    console.log(notes.value)
+    return notes.value
+  } else console.log('error, cannot get notes')
+}
+
+//DELETE
+const removeNote = async (removeNoteId) => {
+  const res = await fetch(`http://localhost:5000/notes/${removeNoteId}`, {
+    method: 'DELETE'
+  })
+  if (res.status === 200) {
+    notes.value = notes.value.filter((note) => note.id !== removeNoteId)
+    console.log('deleted successfullly')
+  } else console.log('error, cannot delete')
+}
+
+// CREATE
+const createNewNote = async (newNote) => {
+  const res = await fetch('http://localhost:5000/notes', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({ noteDetail: newNote })
+  })
+  if (res.status === 201) {
+    const addedNote = await res.json()
+    notes.value.push(addedNote)
+
+    console.log('created successfully')
+  } else console.log('error, cannot create')
+}
+// EDIT
+const editingNote = ref({})
+const toEditingMode = (editNote) => {
+  editingNote.value = editNote
+  console.log(editingNote.value)
+}
+const modifyNote = async (editingNote) => {
+  const res = await fetch(`http://localhost:5000/notes/${editingNote.id}`, {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      noteDetail: editingNote.noteDetail
+    })
+  })
+
+  if (res.status === 200) {
+    const modifyNote = await res.json()
+    notes.value = notes.value.map((note) =>
+      note.id === modifyNote.id
+        ? { ...note, noteDetail: modifyNote.noteDetail }
+        : note
+    )
+
+    console.log('edited successfully')
+  } else console.log('error, cannot edit')
+}
+</script>
+
+<template>
+  <div>
+    <CreateEditNote
+      @createNote="createNewNote"
+      :currentNote="editingNote"
+      @updateNote="modifyNote"
+    />
+    <NoteList
+      :noteList="notes"
+      @deleteNote="removeNote"
+      @editNote="toEditingMode"
+    />
+  </div>
+</template>
+
+<style></style>
---
+++ basic-fetch-notes/src/components/NoteList-G2.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineEmits(['deleteNote', 'editNote'])
+defineProps({
+  noteList: {
+    type: Array,
+    require: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h2>Note List</h2>
+    <ul>
+      <li v-for="(note, index) in noteList" :key="index">
+        Note Id: {{ note.id }}, Note Detail: {{ note.noteDetail }}
+        <button @click="$emit('editNote', note)">Edit</button>
+        <button @click="$emit('deleteNote', note.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
